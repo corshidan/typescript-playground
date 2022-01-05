93ba1283b6b3388256f60af2cfd55f34e09c3d59
corshi.dan <corshi.dan@gmail.com>
2022-01-05 13:10:32 +0000

    updated

--- react-quiz/src/components/QuestionCard.styles.ts
+++ react-quiz/src/components/QuestionCard.styles.ts
@@ -1,4 +1,10 @@
 import styled from 'styled-components';
+
+type ButtonWrapperProps = {
+	correct: boolean;
+	userClicked: boolean;
+};
+
 export const Wrapper = styled.div`
 	max-width: 1100px;
 	background: #ebfeff;
@@ -12,11 +18,6 @@ export const Wrapper = styled.div`
 		font-size: 1rem;
 	}
 `;
-
-type ButtonWrapperProps = {
-	correct: boolean;
-	userClicked: boolean;
-};
 
 export const ButtonWrapper = styled.div<ButtonWrapperProps>`
 	transition: all 0.3s ease;
